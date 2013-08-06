34ec04f92ec4b154d8cd2459f0a47c9fa60be91f
TripNRaVeR <noerialexeew@live.nl>
2013-08-06 19:28:49 +0200

    cpuinit: get rid of __cpuinit, first regexp
    Conflicts:
    	arch/arm/mach-msm/acpuclock-krait.c
    	arch/arm/mach-msm/cpufreq.c
    	arch/arm/mach-msm/msm_mpdecision.c
    	drivers/tripndroid/cpu_management/tdf_hotplug.c
    	kernel/cpu.c

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -255,7 +255,8 @@ out:
 EXPORT_SYMBOL(cpu_down);
 #endif 
 
-static int __cpuinit _cpu_up(unsigned int cpu, int tasks_frozen)
+/* Requires cpu_add_remove_lock to be held */
+static int _cpu_up(unsigned int cpu, int tasks_frozen)
 {
 	int ret, nr_calls = 0;
 	void *hcpu = (void *)(long)cpu;
@@ -291,7 +292,7 @@ out_notify:
 }
 
 extern void trace_cpu_up_frequency (unsigned int cpu);
-int __cpuinit cpu_up(unsigned int cpu)
+int cpu_up(unsigned int cpu)
 {
 	int err = 0;
 
@@ -491,7 +492,7 @@ core_initcall(cpu_hotplug_pm_sync_init);
 
 #endif 
 
-void __cpuinit notify_cpu_starting(unsigned int cpu)
+void notify_cpu_starting(unsigned int cpu)
 {
 	unsigned long val = CPU_STARTING;
 
